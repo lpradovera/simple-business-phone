1a5c476b39aef4bc7e3094015f1133b0b6d55d49
Luca Pradovera <luca.pradovera@gmail.com>
2023-04-17 16:59:05 +0200

    Working app

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+let express = require('express');
+let app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const axios = require('axios');
+
+async function apiRequest(endpoint, payload = {}, method = 'POST') {
+  var url = `https://${process.env.SIGNALWIRE_SPACE}${endpoint}`
+
+  resp = await axios.post(url, payload, {
+    auth: {
+      username: process.env.SIGNALWIRE_PROJECT_KEY,
+      password: process.env.SIGNALWIRE_TOKEN
+    }
+  })
+  return resp.data
+}
+
+app.post('/token', async (req, res) => {
+  var defaultDestination = process.env.DEFAULT_DESTINATION
+  var projectId = process.env.SIGNALWIRE_PROJECT_KEY
+  var resource = req.body.resource || 'myclient'
+  var token = await apiRequest('/api/relay/rest/jwt', { expires_in: 120, resource: resource }) 
+  res.json(token)
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running on port 3000");
+});
+
+
